209c349b7430452320594189eb525a1603f34915
d.konofolskiy <d.konofolskiy@yclients.tech>
2025-08-03 21:59:34 +0300

    test

--- project/tests/test_login.py
+++ project/tests/test_login.py
@@ -1,18 +1,19 @@
 # project/tests/test_login.py
 import pytest
-from pages.login_page import LoginPage
-from pages.dashboard_page import DashboardPage
+
+
+def test_login_failure(login_page):
+    login_page.navigate()
+    login_page.login('user', 'password')
+
+    assert login_page.get_error_message() == 'Invalid credentials. Please try again.'
 
 
 @pytest.mark.parametrize('username, password', [
     ('user', 'user'),
     ('admin', 'admin')
 ])
-def test_login_success(page, username, password):
-    login_page = LoginPage(page)
-    dashboard_page = DashboardPage(page)
-
+def test_login_success(login_page, dashboard_page, username, password):
     login_page.navigate()
-    login_page.login(username, password)
-
+    login_page.login(username,password)
     dashboard_page.assert_welcome_message(f"Welcome {username}")
